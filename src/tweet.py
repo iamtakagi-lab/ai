f1302eccdebc956d39b2a798575f383d8c2f2b60
yude <i@yude.jp>
2022-10-18 23:17:56 +0900

    Add filter etc.

--- src/tweet.py
+++ src/tweet.py
@@ -3,8 +3,19 @@ from generateModel import generate_model
 from constants import API
 from makeSentence import make_sentence
 
+def remover(result):
+  # Load banned.json
+  json_open = open('data/banned.json', 'r')
+  json_load = json.load(json_open)
+  for w in json_load['words']:
+    result = result.replace(w, '')
+  return result
+
 def tweet():
     fetch_tweets()
     generate_model()
-    API.update_status(status = make_sentence())
-    API.update_status(status = make_sentence())
+    API.update_status(
+            status = remover(
+                    make_sentence()
+                )
+        )
