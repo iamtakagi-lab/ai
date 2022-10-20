f7b313a41b13fd577c8191a47bd88601ad9d3285
yude <i@yude.jp>
2022-10-21 05:38:24 +0900

    feat: randomly choose tweet method

--- src/tweet.py
+++ src/tweet.py
@@ -3,11 +3,21 @@ from generateModel import generate_model
 from constants import API
 from makeSentence import make_sentence
 
+import numpy as np
 import json
 
 def tweet():
-    fetch_tweets()
-    generate_model()
-    API.update_status(
-            status = make_sentence()
-        )
+    # generate tweet by using markov or original tweet
+    selection = np.random.choice(["markov", "original"], p=["0.95", "0.05"])
+    
+    if selection == "markov":
+        fetch_tweets()
+        generate_model()
+
+        API.update_status(
+                status = make_sentence()
+            )
+    else:
+        API.update_status(
+                status = make_sentence()
+            )
